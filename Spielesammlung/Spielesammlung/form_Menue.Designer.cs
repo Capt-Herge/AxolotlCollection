b3ecdeb167886c8715f7b7d7e018821ce3a5031e
Hergen <capt_herge@freenet.de>
2017-12-13 11:07:49 +0100

    1. Upload

---
+++ Spielesammlung/Spielesammlung/form_Menue.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Spielesammlung
+{
+    partial class form_Menue
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zurückZumMenüToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseWeiterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.neustartenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.programmBeendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.highscoreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionenToolStripMenuItem,
+            this.highscoreToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1008, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // optionenToolStripMenuItem
+            // 
+            this.optionenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zurückZumMenüToolStripMenuItem,
+            this.pauseWeiterToolStripMenuItem,
+            this.neustartenToolStripMenuItem,
+            this.programmBeendenToolStripMenuItem});
+            this.optionenToolStripMenuItem.Name = "optionenToolStripMenuItem";
+            this.optionenToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.optionenToolStripMenuItem.Text = "Optionen";
+            // 
+            // zurückZumMenüToolStripMenuItem
+            // 
+            this.zurückZumMenüToolStripMenuItem.Name = "zurückZumMenüToolStripMenuItem";
+            this.zurückZumMenüToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.zurückZumMenüToolStripMenuItem.Text = "Zurück zum Menü";
+            // 
+            // pauseWeiterToolStripMenuItem
+            // 
+            this.pauseWeiterToolStripMenuItem.Name = "pauseWeiterToolStripMenuItem";
+            this.pauseWeiterToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pauseWeiterToolStripMenuItem.Text = "Pause/Weiter";
+            // 
+            // neustartenToolStripMenuItem
+            // 
+            this.neustartenToolStripMenuItem.Name = "neustartenToolStripMenuItem";
+            this.neustartenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.neustartenToolStripMenuItem.Text = "Neustarten";
+            // 
+            // programmBeendenToolStripMenuItem
+            // 
+            this.programmBeendenToolStripMenuItem.Name = "programmBeendenToolStripMenuItem";
+            this.programmBeendenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.programmBeendenToolStripMenuItem.Text = "Programm beenden";
+            // 
+            // highscoreToolStripMenuItem
+            // 
+            this.highscoreToolStripMenuItem.Name = "highscoreToolStripMenuItem";
+            this.highscoreToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.highscoreToolStripMenuItem.Text = "Highscore";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 300);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 120);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Donkey Kong";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(226, 300);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 120);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Pong";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(420, 300);
+            this.button3.Margin = new System.Windows.Forms.Padding(0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 120);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Vanguard";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(614, 300);
+            this.button4.Margin = new System.Windows.Forms.Padding(0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(184, 120);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Snake";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(808, 300);
+            this.button5.Margin = new System.Windows.Forms.Padding(0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(184, 120);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Tic Tac Toe";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(32, 430);
+            this.button6.Margin = new System.Windows.Forms.Padding(0);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(184, 120);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Breakout";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(226, 430);
+            this.button7.Margin = new System.Windows.Forms.Padding(0);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(184, 120);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Minesweeper";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(420, 430);
+            this.button8.Margin = new System.Windows.Forms.Padding(0);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(184, 120);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "4 Gewinnt";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(614, 430);
+            this.button9.Margin = new System.Windows.Forms.Padding(0);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(184, 120);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Astroids";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(808, 430);
+            this.button10.Margin = new System.Windows.Forms.Padding(0);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(184, 120);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Flappy Bird";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1024, 768);
+            this.MinimumSize = new System.Drawing.Size(1024, 768);
+            this.Name = "Form1";
+            this.Text = "Axolotl Collection";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zurückZumMenüToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pauseWeiterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem neustartenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem programmBeendenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem highscoreToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
