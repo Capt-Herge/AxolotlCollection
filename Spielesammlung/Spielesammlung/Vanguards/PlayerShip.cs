7a00dbcfa7134e249df1b3de27f66d0907b1f1bb
Roman Putilov <roman.putilov@googlemail.com>
2017-12-18 22:24:07 +0100

    Änderung von Vanguards in ein WinForm Hinzufügen von Resourscen Anwenden von D2D

---
+++ Spielesammlung/Spielesammlung/Vanguards/PlayerShip.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SlimDX;
+
+//Some namespace mappings
+using D2D = SlimDX.Direct2D;
+using DXGI = SlimDX.DXGI;
+
+namespace Spielesammlung.Vanguards
+{
+    class PlayerShip
+    {
+        private int _posX = 0;
+        private int _posY = 0;
+        private D2D.Bitmap spaceshipBitmap;
+        private Bitmap spaceshipBitmapGDI;
+        private int shipHitboxX = 100;
+        private int shipHitboxY = 100;
+
+        public PlayerShip(D2D.Bitmap spaceshipBitmap)
+        {
+            this.spaceshipBitmap = spaceshipBitmap;
+        }
+
+        public int PosX { get => _posX; set => _posX = value; }
+        public int PosY { get => _posY; set => _posY = value; }
+        public int ShipHitboxX { get => shipHitboxX;}
+        public int ShipHitboxY { get => shipHitboxY; }
+
+        public D2D.Bitmap getShipBitmap()
+        {
+            return spaceshipBitmap;
+        }
+
+
+    }
+}
--- Spielesammlung/Spielesammlung/Vanguards/Vanguards.cs
+++ Spielesammlung/Spielesammlung/Vanguards/Vanguards.cs
@@ -1,75 +1,389 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using SlimDX.Windows;
 using System.Windows.Forms;
 using SlimDX;
-using SlimDX.D3DCompiler;
-using SlimDX.Direct3D11;
-using SlimDX.DXGI;
-using SlimDX.Windows;
-using Device = SlimDX.Direct3D11.Device;
-using Resource = SlimDX.Direct3D11.Resource;
+
+//Some namespace mappings
+using D2D = SlimDX.Direct2D;
+using DXGI = SlimDX.DXGI;
+using System.Threading;
+using System.Windows.Input;
+
 namespace Spielesammlung.Vanguards
 {
-    class Vanguards
+    public partial class Vanguards : Form
     {
-        Device device;
-        SwapChain swapChain;
-        ShaderSignature inputSignature;
-        VertexShader vertexShader;
-        PixelShader pixelShader;
-        RenderForm gWindow = new RenderForm("Vanguard Test");
+        //Resources for Direct2D rendering
+        private D2D.Factory m_factory;
+        private D2D.WindowRenderTarget m_renderTarget;
+        private D2D.LinearGradientBrush m_backBrushEx;
+        private D2D.GradientStopCollection m_backBrushGradient;
+        private D2D.Bitmap m_puzzleBitmap;
+        private D2D.Bitmap spaceShipBitmap;
+        //Some generic members
+        private Bitmap m_puzzleBitmapGdi;
+        private Bitmap spaceshipBitmapGDI;
+        private Brush m_backBrushGdi;
+        private bool m_initialized;
+        private bool m_debugMode;
+        private PlayerShip _Player;
+        private Thread _PlayerControll;
+        private bool _KeyPressed = false ;
+        private String _Taste;
+        private System.Windows.Forms.Timer _movementTimer = new System.Windows.Forms.Timer { Interval = 15 };
 
-        
-
+        private static bool[] keys_down;
+        private static Keys[] key_props;
         public Vanguards()
         {
-            StartWindow();
+            InitializeComponent();
+            keys_down = new bool[4];
+            key_props = new[] { Keys.A, Keys.D, Keys.W, Keys.S };
+            //Update control styles
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.Opaque, true);
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+
+            //Update back brush
+            m_backBrushGdi = new SolidBrush(this.BackColor);
+
+            InitializeGraphics();
+            _movementTimer.Tick += tick;
+            _movementTimer.Start();
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(Vanguards_KeyDown);
+            this.KeyUp += new KeyEventHandler(Vanguards_KeyUp);
+
         }
 
-
-        public void StartWindow()
+        private void tick(Object source, EventArgs e)
         {
+            // Do this every timing interval.
+            _DoMovement();
+            byte n = 0;
+            foreach (var v in keys_down)
+            {
+                if (n == 3 && v)
+                // If the "s" key is being held down, no key delay issues. :)
+            n++;
+            }
+        
+    }
+
+        private void movementTimer_Tick(object sender, EventArgs e)
+        {
+            _DoMovement();
+        }
+
+        private void _DoMovement()
+        {
+            if (_KeyPressed)
+            {
+                _Player.PosY = _Player.PosY+1;
+                this.Invalidate();
+
+            }
+
             
 
-            var description = new SwapChainDescription()
-            {
-                BufferCount = 2,
-                Usage = Usage.RenderTargetOutput,
-                OutputHandle = form.Handle,
-                IsWindowed = true,
-                ModeDescription = new ModeDescription(0, 0, new Rational(60, 1), Format.R8G8B8A8_UNorm),
-                SampleDescription = new SampleDescription(1, 0),
-                Flags = SwapChainFlags.AllowModeSwitch,
-                SwapEffect = SwapEffect.Discard
-            };
-
-            Device.CreateWithSwapChain(DriverType.Hardware, DeviceCreationFlags.Debug, description, out device, out swapChain);
-            RenderTargetView renderTarget;
-            using (var resource = Resource.FromSwapChain<Texture2D>(swapChain, 0))
-                renderTarget = new RenderTargetView(device, resource);
-            var context = device.ImmediateContext;
-            var viewport = new Viewport(0.0f, 0.0f, gWindow.ClientSize.Width, gWindow.ClientSize.Height);
-            context.OutputMerger.SetTargets(renderTarget);
-            context.Rasterizer.SetViewports(viewport);
-            using (var factory = swapChain.GetParent<Factory>())
-                factory.SetWindowAssociation(gWindow.Handle, WindowAssociationFlags.IgnoreAltEnter);
-
-            // handle alt+enter ourselves
-            gWindow.KeyDown += (o, e) =>
-            {
-                if (e.Alt && e.KeyCode == Keys.Enter)
-                    swapChain.IsFullScreen = !swapChain.IsFullScreen;
-            };
-            MessagePump.Run(gWindow, () =>
-            {
-                // clear the render target to a soothing blue
-                context.ClearRenderTargetView(renderTarget, new Color4(0.5f, 0.5f, 1.0f));
-                swapChain.Present(0, PresentFlags.None);
-            });
         }
+
+        private void Vanguards_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            _KeyPressed = false;
+            _movementTimer.Stop();
+            
+        }
+
+        /// <summary>
+        /// Loads a Direct2D bitmap from the given file.
+        /// </summary>
+        /// <param name="file">Source file name.</param>
+        public D2D.Bitmap LoadBitmap(string file)
+        {
+            D2D.Bitmap result = null;
+
+            //Create Gdi Bitmap 
+            Bitmap drawingBitmap = Bitmap.FromFile(file) as Bitmap;
+
+            //Load the Direct2D resource from the gdi resource
+            result = LoadBitmap(drawingBitmap);
+
+            //Dispose the gdi bitmap after loading
+            drawingBitmap.Dispose();
+
+            return result;
+        }
+
+        /// <summary>
+        /// Load a Direct2D bitmap from the given gdi resource.
+        /// </summary>
+        /// <param name="drawingBitmap">The gdi resource.</param>
+        public D2D.Bitmap LoadBitmap(Bitmap drawingBitmap)
+        {
+            D2D.Bitmap result = null;
+
+            //Lock the gdi resource
+            BitmapData drawingBitmapData = drawingBitmap.LockBits(
+                new Rectangle(0, 0, drawingBitmap.Width, drawingBitmap.Height),
+                ImageLockMode.ReadOnly, PixelFormat.Format32bppPArgb);
+
+            //Prepare loading the image from gdi resource
+            DataStream dataStream = new DataStream(
+                drawingBitmapData.Scan0,
+                drawingBitmapData.Stride * drawingBitmapData.Height,
+                true, false);
+            D2D.BitmapProperties properties = new D2D.BitmapProperties();
+            properties.PixelFormat = new D2D.PixelFormat(
+                DXGI.Format.B8G8R8A8_UNorm,
+                D2D.AlphaMode.Premultiplied);
+
+            //Load the image from the gdi resource
+            result = new D2D.Bitmap(
+                m_renderTarget,
+                new Size(drawingBitmap.Width, drawingBitmap.Height),
+                dataStream, drawingBitmapData.Stride,
+                properties);
+
+            //Unlock the gdi resource
+            drawingBitmap.UnlockBits(drawingBitmapData);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Loads all graphics resources.
+        /// </summary>
+        private void InitializeGraphics()
+        {
+            //Get requested debug level
+            D2D.DebugLevel debugLevel = D2D.DebugLevel.None;
+            if (m_debugMode) { debugLevel = D2D.DebugLevel.Error; }
+
+            //Create factory object
+            m_factory = new D2D.Factory(D2D.FactoryType.SingleThreaded, debugLevel);
+
+            //Create the render target
+            m_renderTarget = new D2D.WindowRenderTarget(m_factory, new D2D.WindowRenderTargetProperties()
+            {
+                Handle = this.Handle,
+                PixelSize = this.Size,
+                PresentOptions = D2D.PresentOptions.Immediately
+            });
+
+            //Create linear gradient brush
+            D2D.GradientStop[] gradientStops = new D2D.GradientStop[]
+            {
+                new D2D.GradientStop(){ Position = 0f, Color = new Color4(Color.LightGray) },
+                new D2D.GradientStop(){ Position = 1f, Color = new Color4(Color.LightSteelBlue) }
+            };
+            m_backBrushGradient = new D2D.GradientStopCollection(m_renderTarget, gradientStops);
+            m_backBrushEx = new D2D.LinearGradientBrush(
+                m_renderTarget,
+                m_backBrushGradient,
+                new D2D.LinearGradientBrushProperties()
+                {
+                    StartPoint = new PointF(0, this.Height),
+                    EndPoint = new PointF(0, 0)
+                });
+
+            //Create gdi backbrush
+            if (m_backBrushGdi != null) { m_backBrushGdi.Dispose(); }
+            m_backBrushGdi = new LinearGradientBrush(
+                new Point(0, this.Height),
+                new Point(0, 0),
+                Color.LightGray,
+                Color.LightSteelBlue);
+
+            //Load the bitmap
+            m_puzzleBitmapGdi = Properties.Resources.Puzzle;
+            m_puzzleBitmap = LoadBitmap(m_puzzleBitmapGdi);
+            spaceshipBitmapGDI = Properties.Resources.GreenSpaceShip;
+            spaceShipBitmap = LoadBitmap(spaceshipBitmapGDI);
+            _Player = new PlayerShip(spaceShipBitmap);
+            
+
+            //Update initialization flag
+            m_initialized = true;
+        }
+
+        private void UnloadGraphics()
+        {
+            if (m_puzzleBitmap != null) { m_puzzleBitmap.Dispose(); }
+            if (m_backBrushEx != null) { m_backBrushEx.Dispose(); }
+            if (m_backBrushGradient != null) { m_backBrushGradient.Dispose(); }
+            if (m_renderTarget != null) { m_renderTarget.Dispose(); }
+            if (m_factory != null) { m_factory.Dispose(); }
+
+            m_backBrushEx = null;
+            m_backBrushGradient = null;
+            m_renderTarget = null;
+            m_factory = null;
+        }
+
+        /// <summary>
+        /// Called when window opens.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs"/> that contains the event data.</param>
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+
+            if ((!m_initialized) || (!this.DesignMode))
+            {
+                InitializeGraphics();
+            }
+        }
+
+        /// <summary>
+        /// Called when window closes.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs"/> that contains the event data.</param>
+        protected override void OnHandleDestroyed(EventArgs e)
+        {
+            base.OnHandleDestroyed(e);
+
+            if (m_initialized)
+            {
+                UnloadGraphics();
+            }
+        }
+
+        /// <summary>
+        /// Called when background color has changed.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs"/> that contains the event data.</param>
+        protected override void OnBackColorChanged(EventArgs e)
+        {
+            base.OnBackColorChanged(e);
+
+            //Update back brush
+            if (m_backBrushGdi != null) { m_backBrushGdi.Dispose(); }
+            m_backBrushGdi = new SolidBrush(this.BackColor);
+        }
+
+        /// <summary>
+        /// Called when user changes a parameter.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        private void OnChkParameterCheckedChanged(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+        
+        private void Vanguards_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+
+
+                switch (e.KeyCode)
+            {
+              case Keys.S:
+
+                    //    _Player.PosY=_Player.PosY+1;
+                    //     this.Invalidate();
+                    _KeyPressed = true;
+                    //_PlayerControll = new Thread(ShipDown);
+                    //_PlayerControll.Start();
+                    
+              break;
+
+                case Keys.Down:
+                    _Player.PosY = _Player.PosY + 1;
+                    this.Invalidate();
+              break;
+                case Keys.W:
+
+                    _Player.PosY = _Player.PosY - 1;
+                    this.Invalidate();
+                    break;
+
+                case Keys.Up:
+                    _Player.PosY = _Player.PosY - 1;
+                    this.Invalidate();
+                    break;
+                case Keys.D:
+                    _KeyPressed = true;
+                    _Player.PosX = _Player.PosX + 1;
+                    this.Invalidate();
+                    break;
+
+                case Keys.A:
+
+                    _Player.PosY = _Player.PosX - 1;
+                    this.Invalidate();
+                    break;
+
+                case Keys.Left:
+                    _Player.PosY = _Player.PosX - 1;
+                    this.Invalidate();
+                    break;
+
+            }
+            _DoMovement();
+            _movementTimer.Start();
+        }
+        
+        private void ShipDown()
+        {
+            
+            _Player.PosY = _Player.PosY + 1;
+            this.Invalidate();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if(m_initialized)
+            {
+                m_renderTarget.BeginDraw();
+                try
+                {
+                    m_renderTarget.Clear(new Color4(this.BackColor));
+
+                    //Perform all Direct2D rendering here
+                    m_renderTarget.FillRectangle(
+                        m_backBrushEx,
+                        new Rectangle(new Point(), this.Size));
+
+                    //Apply quality
+                    D2D.InterpolationMode interpolationMode = D2D.InterpolationMode.NearestNeighbor;
+                    interpolationMode = D2D.InterpolationMode.Linear; 
+
+                    //Draw all images
+                    int imageCount = 0;
+ 
+                    Stopwatch stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    m_renderTarget.DrawBitmap(m_puzzleBitmap);
+                    m_renderTarget.DrawBitmap(_Player.getShipBitmap(), new RectangleF(_Player.PosX, _Player.PosY, _Player.ShipHitboxX, _Player.ShipHitboxY), 1f, interpolationMode);
+                    stopwatch.Stop();
+
+                }
+                finally
+                {
+                    m_renderTarget.EndDraw();
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Is debug mode enabled?
+        /// </summary>
+        [Category("Direct2D")]
+        public bool IsDebugEnabled
+        {
+            get { return m_debugMode; }
+            set { m_debugMode = value; }
+        }
+
     }
 }
