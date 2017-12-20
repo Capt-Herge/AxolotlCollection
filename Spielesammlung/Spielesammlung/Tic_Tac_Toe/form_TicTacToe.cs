8e9aa9037efffc00577430b36a1c5541076e11c3
Hergen <capt_herge@freenet.de>
2017-12-20 12:04:16 +0100

    Fertigstellen UI Tic Tac Toe

--- Spielesammlung/Spielesammlung/Tic_Tac_Toe/form_TicTacToe.cs
+++ Spielesammlung/Spielesammlung/Tic_Tac_Toe/form_TicTacToe.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Spielesammlung.TicTacToe
 {
-    public partial class Form1 : Form
+    public partial class form_TicTacToe : Form
     {
-        public Form1()
+        public form_TicTacToe()
         {
             InitializeComponent();
         }
